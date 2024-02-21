b096669b988c0aeefca7e2bd8752fea08b0e7e7f
Lucas Medina <lucasmedinarmc@gmail.com>
2024-02-21 19:55:43 -0300

    chore: add shadcnUi

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 function App() {
   return (
-    <div className="my-12 px-5">
-      <h1 className="text-3xl font-bold ">Hello World</h1>
+    <div className="">
+      <h1 className="">Hello World</h1>
     </div>
   )
 }
