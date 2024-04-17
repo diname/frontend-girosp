c69584ab2fd82fd14f7d2637e99c3fc837078cce
Lucas Medina <lucasmedinarmc@gmail.com>
2024-04-17 19:44:24 -0300

    refactor: add form size adjustment

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -7,8 +7,8 @@ import { Button } from '@/components/ui/button'
 export const UserLogin = () => {
   return (
     <div className="bg-orange-400 min-w-full min-h-full flex flex-col justify-center items-center">
-      <div className="p-5">
-        <img src={Logo} alt="Logo da empresa GiroSP" className="w-64 h-32" />
+      <div className="p-5 flex flex-col max-w-96">
+        <img src={Logo} alt="Logo da empresa GiroSP" className="" />
         <form action="" className="flex flex-col md:w-96">
           <Label className="py-2 w-full ">Email: </Label>
           <Input className="py-2 mb-4 w-full" placeholder="Digite seu email" />
